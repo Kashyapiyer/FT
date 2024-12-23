3dc837ff4eb69c4f1718cf4fd6e6d1ab02576d98
K <k.iyer2611@gmail.com>
2024-12-23 14:58:46 -0600

    Update rbrtainferencecnf.py

--- Toxicity/rbrtainferencecnf.py
+++ Toxicity/rbrtainferencecnf.py
@@ -29,13 +29,10 @@ def rbrtatoxiccnfthrvalidator(contextstr, mdlpath=OUTPUT_DIR, threshold=0.020, s
                 #Tokenize inputs
                 inputs = tokenizer(contextstr, return_tensors="pt")
                 output = ft_model(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'])
-
                 #print(output[0][0][0], output[0][0][1])
                 cnf_intr = torch.abs(abs(output[0][0][1]) - abs(output[0][0][0]))
-
                 cnf_intrstr = f"{cnf_intr.item():.3f}"
                 print(f"Abs diff confidence interval: {cnf_intr.item():.3f}")
-
                 # get the outputtensor
                 evallabel = output[0].argmax().item() 
                 cnfthreshold_analysis = (cnf_intrstr>=threshold)
@@ -43,8 +40,7 @@ def rbrtatoxiccnfthrvalidator(contextstr, mdlpath=OUTPUT_DIR, threshold=0.020, s
                 result['evallabel'] = evallabel
                 result['cntthreholdanalysis'] = cnfthreshold_analysis
 
-                if cnfthreshold_analysis: 
-                   
+                if cnfthreshold_analysis:  
                   if evallabel==1:
                     result['prediction'] = 'toxic'
                   else:
